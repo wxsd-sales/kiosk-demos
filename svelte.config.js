f48835031bad7537794581f278d0cc9fba4d233f
William Mills <wmpmills@gmail.com>
2023-01-22 19:52:49 +0000

    Adding new yaml and config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.env.npm_lifecycle_event === "dev";
+
+console.log('ENV: ' + JSON.stringify(process.env))
+
+let base =  process.env.npm_package_name;
+
+if(process.env.REPO_NAME) {
+	console.log('Repo Name Present: ' + process.env.REPO_NAME)
+	base = process.env.REPO_NAME
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,7 +19,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? "" : `/${base}`,
+		},
 	}
 };
 
