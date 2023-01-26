daf77fd44a4739fef9bed191e2e545db07ac140e
William Mills <wmpmills@gmail.com>
2023-01-26 15:19:42 +0000

    Updating static build info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+    import type { LayoutData } from './$types';
     import '../app.scss';
+    export let data: LayoutData;
 </script>
 
 <slot/>
+
+
+
+
--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ let urlBase =  '';
 if(process.env.GITHUB_PAGES == "true") {
 	// Use repo name as base if given, otherwise use package name
 	if(process.env.REPO_NAME) {
-		console.log('Using provide repo name as base: ' + process.env.REPO_NAME)
+		console.log('Using provided repo name as base: ' + process.env.REPO_NAME)
 		urlBase = `/${process.env.REPO_NAME}`;
 	} else {
 		console.log('Using package name as base: ' + process.env.npm_package_name)
@@ -20,12 +20,12 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: 'index.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			strict: true
 		}),
 		paths: {
 			base: urlBase,
