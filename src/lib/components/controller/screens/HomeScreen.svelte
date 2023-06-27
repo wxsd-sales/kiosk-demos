682ef5dc10303f4e4c934d7422599c91e5375e73
William Mills <wmpmills@gmail.com>
2023-06-27 20:55:18 +0200

    Update HomeScreen.svelte

--- src/lib/components/controller/screens/HomeScreen.svelte
+++ src/lib/components/controller/screens/HomeScreen.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import background from './images/1-Night.png';
+	//import background from './images/1-Night.png';
 
 	//import top, widget and bottom
 </script>
 
-<div id="wallpaper" style="background-image: url({background})">
-	<!-- Top Bar
+<!--
+	<div id="wallpaper" style="background-image: url({background})">
+	 Top Bar
 
 	Widget
 
-	Bottom Bar -->
+	Bottom Bar
 </div>
+-->
 
 <style>
 	#wallpaper {
