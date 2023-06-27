ce41cdf0efa3d2a3e99bfea2ad915ab7febb8b66
William Mills <wmpmills@gmail.com>
2023-06-27 20:57:04 +0200

    Removed Background Image

--- src/lib/components/controller/screens/ControlPanel.svelte
+++ src/lib/components/controller/screens/ControlPanel.svelte
@@ -1,18 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import background from './images/1-Night.png';
+	//import background from './images/1-Night.png';
 
 	//import top, widget and bottom
 </script>
 
-<div id="wallpaper" style="background-image: url({background})">
-	<!-- Top Bar
 
-	Widget
-
-	Bottom Bar -->
-</div>
 
 <style>
 	#wallpaper {
