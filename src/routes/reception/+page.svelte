40a49e5b8dba95e047a251bef4afeb5f8fef350f
William Mills <wmpmills@gmail.com>
2023-09-22 12:52:51 +0100

    Update +page.svelte

--- src/routes/reception/+page.svelte
+++ src/routes/reception/+page.svelte
@@ -4,26 +4,27 @@
 	import WebexLogo from '$assets/webex-logo.svg';
 
 	let video: string | null;
-	let spaces: string | null;
+	let spacesToken: string | null;
+	let spacesRegion: string | null;
 	let ready = false;
 
 	onMount(() => {
 		video = $page.url.searchParams.get('video');
-		spaces = $page.url.searchParams.get('spaces');
+		spacesToken = $page.url.searchParams.get('spacesToken');
+		spacesRegion = $page.url.searchParams.get('spacesRegion');
 
-		if (video && spaces) {
+		if (video && spacesToken && spacesRegion) {
 			ready = true;
 		}
 	});
 </script>
 
-<section class="hero is-dark is-fullheight">
-	<!-- Hero head: will stick at the top -->
-	<div class="hero-head" />
-	<!-- Hero content: will be in the middle -->
-	<div class="hero-body p-0">
-		
-			{#if !ready}
+{#if !ready}
+	<section class="hero is-dark is-fullheight">
+		<!-- Hero head: will stick at the top -->
+		<div class="hero-head" />
+		<!-- Hero content: will be in the middle -->
+		<div class="hero-body p-0">
 			<div class="container has-text-centered">
 				<div class="column is-half is-offset-one-quarter">
 					<figure class="image is-16by9">
@@ -32,24 +33,25 @@
 				</div>
 				<p class="title is-size-1">Welcome</p>
 			</div>
-			{:else}
-				<iframe
-					title="YouTube"
-					aria-hidden="true"
-					tabindex="-1"
-					src="{video}"
-					hidden
-				/>
-				<iframe style="width: 100vw; height: 100vh" title="Spaces" src={spaces} />
-			{/if}
-		
-		<!-- Hero footer: will stick at the bottom -->
-		<div class="hero-foot" />
-	</div>
-</section>
+			<div class="hero-foot" />
+		</div>
+	</section>
+{:else}
+	<!-- <iframe title="YouTube" aria-hidden="true" tabindex="-1" src={video} hidden /> -->
+	<iframe
+		style=" display: block; width: 100vw; height: 100vh;  border: none"
+		title="Spaces"
+		src="https://workspaces.dnaspaces.{spacesRegion}/#/preview/?token={spacesToken}"
+		scrolling="no"
+	/>
+{/if}
+
+<!-- Hero footer: will stick at the bottom -->
 
 <style>
 	:global(:root) {
 		overflow-y: hidden;
 	}
+
+
 </style>
